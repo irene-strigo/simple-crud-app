5e63a8c67e66c7a8c5bdad02f0a72a813499e901
Irene Strigo <irene.strigo@gmail.com>
2025-05-08 21:13:37 +0200

    feat: add raw put

--- src/app.js
+++ src/app.js
@@ -60,8 +60,31 @@ console.log('its userid request')
         response.setHeader('Content-Type', 'application/json')
         response.end(JSON.stringify(DB));
     }
-    //response.setHeader('Content-Type', 'application/json');
-    //response.end(JSON.stringify(DB));
+
+    if (request.method === 'PUT'&& request.url.match('|users/[0-9]+$|')) {
+        try {
+            let json = "";
+            for await (const chunk of request) {
+                json += chunk;
+            }
+            const resultRequest = JSON.parse(json);
+            let reqId = request.url.split('/').pop()
+            console.log('updateData',resultRequest)
+
+           let target= DB.find((data)=>data.id === reqId)
+            console.log(target)
+           target.name = resultRequest.name;
+            target.age = resultRequest.age;
+            target.hobbies = resultRequest.hobbies;
+            target.id = reqId
+            response.statusCode = 201
+        } catch (err) {
+            console.log(err)
+        }
+        response.setHeader('Content-Type', 'application/json')
+        response.end(JSON.stringify(DB));
+    }
+
 });
 
 server.listen(process.env.PORT, function(){ console.log("Server is started at port:",process.env.PORT)})
